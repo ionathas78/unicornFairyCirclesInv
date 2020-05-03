bc309e1f6565ee1d094e64a662ebcaadf73268e5
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-03 12:32:29 -0500

    testing button

--- server.js
+++ server.js
@@ -23,39 +23,33 @@ app.get("/inv", function(req, res) {
   // sendAjax_CORS(queryString);
   // sendAjax_CORS('https://inventory.zoho.com/api/v1/items?authtoken=${process.env.AUTH}&organization_id=${process.env.ORG}');
 
-  function sendAjax_CORS(queryString) {
-    let queryURL = req.protocol + _CORS_SERVER + queryString;
-    console.log(queryURL);
+  https.get("https" + _CORS_SERVER + queryString, resp => {
+    let itemArray = [];
+    // console.log(resp);
 
-    https.get("https" + _CORS_SERVER + queryString, resp => {
-      let itemArray = [];
-      // console.log(resp);
+    if (!resp || !resp.items) {
+      res.status(503).send("No luck...");
+    };
 
-      if (!resp || !resp.items) {
-        res.status(503).send("No luck...");
-      };
+    for (let i = 0; i < resp.items.length; i++) {
+        let item = resp.items[i];
 
-      for (let i = 0; i < resp.items.length; i++) {
-          let item = resp.items[i];
+        if ((item !== undefined) && 
+                    (item.item_name.toLowerCase().indexOf("buttons") > -1)) {
+            itemArray.push({
+                name: item.item_name,
+                SKU: item.sku,
+                image: "./images/" + item.sku + ".jpg",
+                // image: req.protocol + _PLACEHOLDER_IMAGE,
+                stock: item.available_stock,
+                unit: item.unit
+            });
+        };
+    };
+    
+    itemArray.sort((itemA, itemB) => itemA.sku > itemB.sku);
 
-          if ((item !== undefined) && 
-                      (item.item_name.toLowerCase().indexOf("buttons") > -1)) {
-              itemArray.push({
-                  name: item.item_name,
-                  SKU: item.sku,
-                  image: "./images/" + item.sku + ".jpg",
-                  // image: req.protocol + _PLACEHOLDER_IMAGE,
-                  stock: item.available_stock,
-                  unit: item.unit
-              });
-          };
-      };
-      
-      itemArray.sort((itemA, itemB) => itemA.sku > itemB.sku);
-
-      res.json(JSON.stringify(itemArray));
-    });
-  };
+    res.json(JSON.stringify(itemArray));
 });
 
 
@@ -64,3 +58,37 @@ app.listen(PORT, () => {
   // Log (server-side) when our server has started
   console.log(`Server listening on: https://peaceful-caverns-19894.herokuapp.com:${PORT && process.env.PORT}`);
 });
+
+function sendAjax_CORS(queryString) {
+  let queryURL = req.protocol + _CORS_SERVER + queryString;
+  console.log(queryURL);
+
+  https.get("https" + _CORS_SERVER + queryString, resp => {
+    let itemArray = [];
+    // console.log(resp);
+
+    if (!resp || !resp.items) {
+      res.status(503).send("No luck...");
+    };
+
+    for (let i = 0; i < resp.items.length; i++) {
+        let item = resp.items[i];
+
+        if ((item !== undefined) && 
+                    (item.item_name.toLowerCase().indexOf("buttons") > -1)) {
+            itemArray.push({
+                name: item.item_name,
+                SKU: item.sku,
+                image: "./images/" + item.sku + ".jpg",
+                // image: req.protocol + _PLACEHOLDER_IMAGE,
+                stock: item.available_stock,
+                unit: item.unit
+            });
+        };
+    };
+    
+    itemArray.sort((itemA, itemB) => itemA.sku > itemB.sku);
+
+    res.json(JSON.stringify(itemArray));
+  });
+};
